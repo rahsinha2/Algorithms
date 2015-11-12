0de1cb89b7b4c3891eac2eaf40c65c4305bab20e
Rahul Sinha <rahsinha2@gmail.com>
2015-11-11 19:59:54 -0500

    reverse an Integer

--- Leetcode/reverseInteger.cpp
+++ Leetcode/reverseInteger.cpp
@@ -25,13 +25,11 @@ int reverse(int x)
     
     int reversed = 0;
     
-    if (x!=0)    {
-        while(x!=0)  {
-            if (reversed > (INT_MAX/10) || reversed < (INT_MIN/10))
-                return 0;
-            reversed = (reversed * 10) + (x%10);
-            x = x/10;
-        }
+    while(x!=0)  {
+        if (reversed > (INT_MAX/10) || reversed < (INT_MIN/10))
+            return 0;
+        reversed = (reversed * 10) + (x%10);
+        x = x/10;
     }
     return reversed;    
 }
