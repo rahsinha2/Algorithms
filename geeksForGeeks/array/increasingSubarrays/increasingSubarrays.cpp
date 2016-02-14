d1e433aa43311a02b2550c81c2ae55bc10e75173
Rahul Sinha <rahsinha2@gmail.com>
2016-02-14 16:19:37 -0500

    Count number of stricly increasing subarrays

--- geeksForGeeks/array/increasingSubarrays/increasingSubarrays.cpp
+++ geeksForGeeks/array/increasingSubarrays/increasingSubarrays.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 int countSubarrays(int * array, int size);
+int countSubarrays_optimized(int * array, int size);
 
 int main() {
-	int array[] = {1,2,3,4,5};
+	int array[] = {1,2,8,7,6,9,10};
 	cout<<"\nNumber of Subarrays = "<<countSubarrays(array, (int)sizeof(array)/sizeof(int));	
+	cout<<"\nNumber of Subarrays = "<<countSubarrays_optimized(array, (int)sizeof(array)/sizeof(int));	
 
 
 	return 0;
@@ -27,3 +29,23 @@ int countSubarrays(int * array, int size) {
 	}
 	return count;
 }
+/*	if a1,a2,a3,...an is sorted and has legth len
+ *	then number of strictly increasing subarrays = len*(len-1)/2  
+ *	***************************************************************/
+
+int countSubarrays_optimized(int * array, int size){
+	int count = 0;
+	int len = 1;
+	for (int i=0; i<size-1; i++) {
+		
+		if (array[i+1] > array[i]) 
+			len++;
+		else {
+			count += len*(len-1)/2;
+			len = 1;
+		}
+	}
+	
+	count += len*(len-1)/2; /*this is for last subarray */
+	return count;	
+} 
